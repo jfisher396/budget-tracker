32c12cd5f1bb6e1dbe2e425e0990e927ec7219a0
James Fisher <jfisher396@hotmail.com>
2022-04-02 07:06:27 -0500

    cleaned up server.js file

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-// const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,19 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "//user1:password1>@ds137101.mlab.com:37101/heroku_flnhj3wq", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  });
-  // "mongodb://localhost/budget"
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+// "mongodb://localhost/budget"
 
 // routes
 app.use(require("./routes/api.js"));
-
-// app.get("/", function (req, res) {
-//   res.json(path.join(__dirname, "public/index.html"));
-// });
 
 app.listen(PORT, () => {
   console.log(`Application running on PORT ${PORT}`);
